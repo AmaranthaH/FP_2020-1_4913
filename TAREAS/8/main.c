6aee21f1af2f04ad644ab8933b54df78cbbf109a
AmaranthaH <54244142+AmaranthaH@users.noreply.github.com>
2019-10-29 14:09:33 -0600

    listo

---
+++ TAREAS/8/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+  // x es lo que se quiere sacar
+  //y el valor a convertir
+int x= atoi(argv[1]);
+int y = atoi(argv[2]);
+if(x!=0){
+  //este pasa de fahrenhait a celsius
+float c = (y -32)/1.8;
+printf("%.2lf",c);
+}
+  else{
+    // este es de celsius a fahrenhait
+    float f = (y*1.8)+ 32;
+    printf("%.2lf",f);
+
+  }
+}
