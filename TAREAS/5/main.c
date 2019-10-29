340438571df16d049f6007e9935210764ede80d3
AmaranthaH <54244142+AmaranthaH@users.noreply.github.com>
2019-10-29 13:51:23 -0600

    Update main.c

--- TAREAS/5/main.c
+++ TAREAS/5/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]){
 
 for(int i=1;i<argc;i++){
 char n = argv[i][0];
+	//primero evalua los tipo char
 if(n=='A'){
     printf("%i\n",11);
   }else{
@@ -12,6 +13,8 @@ if(n=='A'){
   	printf("%i\n",10);
   	}
 else {
+	//si no entra en nunguno de estos
+	//se combierte en entero y ahora evalua esos valores
 	n= atoi(argv[i]);
 
   if(n ==2|| n == 3 || n == 4 || n == 5 || n == 6 || n == 7 || n == 8||n ==9|| n == 10 ){
