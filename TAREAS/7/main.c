fc424485657b96ad392cdd9c74e06f46a1f58378
AmaranthaH <54244142+AmaranthaH@users.noreply.github.com>
2019-11-04 16:23:02 -0600

    sin pow

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, char *argv[]) {
 	//tomar los valores que se necesitan, que es la base y a que exponente se quiere aumentar
 int b= atoi(argv[1]); // base
 int e = atoi(argv[2]); //exponente
-int p= pow(b,e); //funcion que realiza la operacion de aumentar ell exponente
+int p= 1;
+//iteracion para sacar el exponente
+for(int i=1;i<=e;i++){
+  p = p * b;
+}
 printf("%i\n",p);//mostrar el exponente
 return 0;
 }
