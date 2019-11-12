4eda5bc72077e114e1f924d7c73be4f8a55e8fcb
AmaranthaH <54244142+AmaranthaH@users.noreply.github.com>
2019-11-12 10:55:55 -0600

    Update main.c

--- TAREAS/12/main.c
+++ TAREAS/12/main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 
 int abrazos(int n){
+//esto sera lo que detenga el programa  puesto su minimo sera 1
   if(n == 1){
     n=0;
     return n;
   }
+//esto ingresa de nuevo a la funcion y deja el valor del momento hasta arriba para que cuando regrese se sume tambien ese valor
   int z = abrazos(n-1)+(n-1);
   return z;
   }
 int main(int argc, char*argv[]){
   int x;
+//esto toma el numero de personas que se considerara
 	x = atoi(argv[1]);
   int b = abrazos(x);
   printf("%i",b);
